6cdd89b90cd57febdb8aed63df9d644455fdce78
Zack <schaxmann@gmail.com>
2022-11-21 16:44:24 +0000

    Week 1 CL4

--- 01-SQL-1/SQL-CL4.sql
+++ 01-SQL-1/SQL-CL4.sql
@@ -1,5 +1,14 @@
 USE uni;
 
 SELECT * FROM application
-WHERE CourseAppliedFor = 11 && accepted = 1 
-WHERE CourseID IS NOT NULL;
+LEFT JOIN student ON application.StudentID=student.StudentID
+WHERE CourseAppliedFor = 11 && accepted = 1 && student.studentID IS NULL;
+
+INSERT INTO student (student.studentID, student.CourseID, student.Forenames, student.Surname, student.EmailAddress)
+SELECT application.studentID, application.CourseAppliedFor, application.Forenames, application.Surname, application.EmailAddress
+FROM application
+LEFT JOIN student ON application.StudentID=student.StudentID
+WHERE CourseAppliedFor = 11 && accepted = 1 && student.studentID IS NULL;
+
+SELECT * FROM application
+WHERE CourseAppliedFor = 11 && accepted = 0;
